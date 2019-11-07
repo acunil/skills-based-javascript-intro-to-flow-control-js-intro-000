7bde843e8e4f0c017c665b2186e51b86ad2afe7a
Lucian Amos <locamos@gmail.com>
2019-11-07 13:28:18 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -18,10 +18,10 @@ if(age >=13 && age<=19){
   return("You are a teenager!");
 }else if(age<=12) {
   return("You are a kid")
-}else{
+}
   return("You are a grownup")
 }
-}
+
 function ternaryTeenager(age) {
 
 }
